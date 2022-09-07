5d900cfbb433a650d0ee8f332bc1a11be8c40088
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-09-06 23:52:46 -0400

    associations added

--- models/index.js
+++ models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'Category_id'
+});
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'Category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'Tag_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey:'Product_id'
+});
 module.exports = {
   Product,
   Category,
