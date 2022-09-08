d37971bef106f02de9b273c3f9984e15ffb8e569
joshua.p.ward2 <joshua.p.ward2@gmail.com>
2022-09-08 18:30:01 -0400

    delete route added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -64,6 +64,23 @@ req.body
 
   router.delete('/:id', (req, res) => {
     // delete a category by its `id` value
+    Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+
   });
 
   module.exports = router;
